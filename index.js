d8eed23e8151088dadc6024473da06a4af9344ac
hafizameerhamza <hafizameerhamza1@gmail.com>
2024-07-21 00:47:17 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import { fromPath } from "pdf2pic";
+import fs from "fs/promises";
+
+
+/* properties to converted files */
+const options = {
+    density: 300,
+    format: "jpg",
+    width: 400,
+    height: 162
+};
+
+try {
+
+    // convert pdf pages and retun into buffer
+    const resolve = await fromPath("sample.pdf", options)
+    .bulk(-1, { responseType: "buffer" });
+
+    // console.log(resolve);
+
+    // store all the pages from buffer to the defined path
+    await Promise.all(resolve.map(async (pageData, index) => {
+        const pagePath = `images/page_${pageData.page}.jpg`;
+
+        await fs.writeFile(pagePath, pageData.buffer);
+
+        console.log(`Page ${pageData.page} saved to ${pagePath}`);
+    }));
+} catch (error) {
+    console.log(error);
+}
